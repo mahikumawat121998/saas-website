87303444de0e5153762d491df1620e351dbef6cc
mahesh-kumawat-9x-dev <mahesh.k@9xtechnology.com>
2026-09-14 23:52:49 +0530

    fix: add output standalone to next.config.ts for opennextjs builder

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: "standalone",
 };
 
 export default nextConfig;
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
